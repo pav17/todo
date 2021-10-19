0d0c9e295b07de56dfd071e8067f3a3c690d18a5
Per Van Dyke <pav17@hampshire.edu>
2021-10-19 13:23:12 -0400

    serve todo list app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const api = express();
 
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, () => {
     console.log('API up and running!');
 });
 
-api.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello World!');
-})
+//api.get('/', (req, res) => {
+//    res.send('Hello World!');
+//})
